5621248d75e09ba2929b04ba5cbc48e66482b8f7
Yann Chapuis <yannc4394@gmail.com>
2023-04-06 14:18:28 +0200

    ajout generator anecdotes

--- src/components/TheAnecdotes.vue
+++ src/components/TheAnecdotes.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import ReloadIcon from '@/components/icons/IconReload.vue'
-import { anecdoteGenerator } from '@/services/anecdoteService'
+import { anecdoteGenerator } from '@/services/anecdote.service.js'
 import { onMounted, ref } from 'vue'
 
 let anecdote = ref('')
@@ -8,7 +8,6 @@ let reloadState = ref(false)
 onMounted(() => {
   reloadNewAnecdote()
 })
-
 async function reloadNewAnecdote() {
   if (reloadState.value) return
   reloadState.value = true
@@ -20,6 +19,13 @@ async function reloadNewAnecdote() {
 <template>
   <span class="text-center mb-10">{{ anecdote }}</span>
   <ReloadIcon :class="reloadState ? 'rotate' : 'cursor-pointer'" @click="reloadNewAnecdote()" />
+  <a
+    class="absolute bottom-0 right-0 h-6 w-6"
+    href="https://github.com/Yann-Chapuis/yannecdotes"
+    target="_blank"
+  >
+    <img src="@/assets/logo-128x128.png" />
+  </a>
 </template>
 
 <style></style>
